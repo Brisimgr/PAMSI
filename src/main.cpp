ccd2192e4ea4d5d4c6a9a40447653b15fa6a7509
Gimlig <tomek.lutkowski@protonmail.com>
2023-04-04 19:18:13 +0200

    Nowy main

--- src/main.cpp
+++ src/main.cpp
@@ -3,103 +3,51 @@
 #include"../inc/stack.hpp"
 #include"../inc/table.hh"
 #include"../inc/queue.hh"
+#include<algorithm>
 
 #include<fstream>
 #include<chrono>
 
 int main()
 {
-    LinkedList<int>* list = new LinkedList<int>();
-    Stack<int>* stos = new Stack<int>(); 
-    Table<int> tablica;
-    Queue* kolejka = new Queue();
-
     std::ifstream myFileRead;
     std::ofstream myFileWrite;
+
+    int const ile = 100;
+    int tablica[ile];
     
     myFileWrite.open("test.txt");
     if(myFileWrite.is_open())
-        generateFile(&myFileWrite, 10);
+        generateFile(&myFileWrite, ile);
     
     myFileWrite.close();
     
     myFileRead.open("test.txt");
 
     int number;
-
-    std::cout << "Czas dodawania elementow do struktury dla 10000 elementow" << std::endl;
-
-    auto begin = std::chrono::high_resolution_clock::now();
+    int i = 0;
 
     if(myFileRead.is_open())
     {
         while(myFileRead.good() == true)
         {
             number = readFile<int>(&myFileRead);
-            list->addAtHead(number);
+            tablica[i] = number;
+            ++i;
         }
-    };
+    }
+
+    auto begin = std::chrono::high_resolution_clock::now();
+
+    // Tutaj wstawcie sortowanie i w cout zmiencie nazwe sortowanie i zapiszcie wyniki dla poszczegolnych sortowan
 
     auto end = std::chrono::high_resolution_clock::now();
     auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
 
-    std::cout << "lista: " << elapsed.count() << "ns" << std::endl;
+    std::cout << "Sortowanie bąbelkowe: " << elapsed.count() << "ns" << std::endl;
 
-    number = 0;
-
-    auto begin2 = std::chrono::high_resolution_clock::now();
-
-    if(myFileRead.is_open())
-    {
-        while(myFileRead.good() == true)
-        {
-            number = readFile<int>(&myFileRead);
-            stos->push(number);
-        }
-    }
-
-    auto end2 = std::chrono::high_resolution_clock::now();
-    auto elapsed2 = std::chrono::duration_cast<std::chrono::nanoseconds>(end2 - begin2);
-
-    std::cout << "stos: " << elapsed2.count() << "ns" << std::endl;
-
-    auto begin3 = std::chrono::high_resolution_clock::now();
-
-    if(myFileRead.is_open())
-    {
-        while(myFileRead.good() == true)
-        {
-            number = readFile<int>(&myFileRead);
-            tablica.push_back(number);
-        }
-    }
-
-    auto end3 = std::chrono::high_resolution_clock::now();
-    auto elapsed3 = std::chrono::duration_cast<std::chrono::nanoseconds>(end3 - begin3);
-
-    std::cout << "tablica: " << elapsed3.count() << "ns" << std::endl;
-
-    auto begin4 = std::chrono::high_resolution_clock::now();
-
-    if(myFileRead.is_open())
-    {
-        while(myFileRead.good() == true)
-        {
-            number = readFile<int>(&myFileRead);
-            kolejka->enqueue(number);
-        }
-    }
-
-    auto end4 = std::chrono::high_resolution_clock::now();
-    auto elapsed4 = std::chrono::duration_cast<std::chrono::nanoseconds>(end4 - begin4);
-
-    std::cout << "kolejka: " << elapsed4.count() << "ns" << std::endl;
 
     myFileRead.close();
-
-    delete(list);
-    delete(stos);
-    delete(kolejka);
 
     return 0;
 }
