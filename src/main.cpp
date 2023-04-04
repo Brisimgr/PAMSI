545c8ef6774b8297f1bdbbe3d169834883e70ff6
Pulsarnixx <marek.mazurczak54@gmail.com>
2023-04-04 22:52:49 +0200

    Dodanie quicksorta

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,23 @@
 #include"../inc/stack.hpp"
 #include"../inc/table.hh"
 #include"../inc/queue.hh"
+#include"../inc/quickSort.hh" 
+
 #include<algorithm>
 
 #include<fstream>
 #include<chrono>
 
+
+
 int main()
 {
+
+   
     std::ifstream myFileRead;
     std::ofstream myFileWrite;
 
-    int const ile = 100;
+    int const ile = 10000;
     int tablica[ile];
     
     myFileWrite.open("test.txt");
@@ -39,13 +45,16 @@ int main()
 
     auto begin = std::chrono::high_resolution_clock::now();
 
+
     // Tutaj wstawcie sortowanie i w cout zmiencie nazwe sortowanie i zapiszcie wyniki dla poszczegolnych sortowan
 
+    quicksort(tablica,0,i-1);
+
     auto end = std::chrono::high_resolution_clock::now();
-    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
 
-    std::cout << "Sortowanie bąbelkowe: " << elapsed.count() << "ns" << std::endl;
-
+    std::cout << "Sortowanie quick sortem: " << elapsed.count() << "ms" << std::endl;
+    //printArray(tablica,i);
 
     myFileRead.close();
 
